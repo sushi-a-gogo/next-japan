f76291c54e71e6c3723310f6c4f4f888ed6e3169
sushi-a-gogo <joe.svatek@icloud.com>
2025-06-25 17:14:44 -0500

    way back

--- frontend/src/app/pages/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,12 @@
 import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ImageService } from '@app/services/image.service';
 import { plans } from './subscription-plan.model';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [NgOptimizedImage, CurrencyPipe],
+  imports: [NgOptimizedImage, RouterLink, CurrencyPipe],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
