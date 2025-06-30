8182e84e28c72d3e4b698f6a5a9f4126bf339b93
sushi-a-gogo <joe.svatek@icloud.com>
2025-06-30 15:29:11 -0500

    formatDate helper

--- frontend/src/app/services/date-time.service.ts
+++ frontend/src/app/services/date-time.service.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { LocationTimeZone } from 'src/app/models/location-time-zone.model';
 
@@ -6,6 +7,10 @@ import { LocationTimeZone } from 'src/app/models/location-time-zone.model';
 })
 export class DateTimeService {
   private readonly locale = 'en-US'; // locale code for the locale format rules to use
+
+  formatDateAsString(date: Date, format: string) {
+    return formatDate(date, format, this.locale);
+  }
 
   adjustDateToTimeZoneOffset(dateString: string, location: LocationTimeZone) {
     const date = new Date(dateString);
