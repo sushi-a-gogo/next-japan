dea5da9ad5389a70631c2b964f181d6086790c0c
sushi-a-gogo <joe.svatek@icloud.com>
2025-07-08 17:35:33 -0500

    ..

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   envName: 'dev',
-  baseUrl: 'http://localhost:4000',
+  baseUrl: 'http://localhost:4200',
   apiUrl: 'http://localhost:3000',
   cloudfareUrl: 'https://imagedelivery.net',
   cloudfareAccountHash: 'OaC0XqahBZ38brk0AdgtLA'
