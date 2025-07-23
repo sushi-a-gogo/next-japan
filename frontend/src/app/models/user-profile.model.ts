076a64c6ca43181361f7486e5d41a912c551b705
sushi-a-gogo <joe.svatek@icloud.com>
2025-07-23 16:15:48 -0500

    ..

--- frontend/src/app/models/user-profile.model.ts
+++ frontend/src/app/models/user-profile.model.ts
@@ -6,9 +6,6 @@ export interface UserProfile extends User {
   state: string | null;
   zip: string | null;
   phone: string | null;
-  createdDate?: Date;
-  lastUpdateDate?: Date;
-  additionalInformation?: string;
   isEmailPreferred: boolean;
   mode?: 'light' | 'dark';
 }
