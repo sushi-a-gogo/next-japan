05f0deb80efab6b734c404b94a4fb0f5934080fb
sushi-a-gogo <joe.svatek@icloud.com>
2025-08-14 12:30:41 -0500

    Update site url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   envName: 'production',
-  baseUrl: 'https://next-japan-web.onrender.com',
+  baseUrl: 'https://nextjapan.jotek.dev',
   apiUrl: 'https://next-japan-api.onrender.com',
   cloudfareUrl: 'https://imagedelivery.net',
   cloudfareAccountHash: 'OaC0XqahBZ38brk0AdgtLA'
