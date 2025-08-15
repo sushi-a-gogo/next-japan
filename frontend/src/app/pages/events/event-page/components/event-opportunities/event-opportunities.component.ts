60863b658db4c900b98054b4e982ce858b6345b1
sushi-a-gogo <joe.svatek@icloud.com>
2025-08-15 16:20:35 -0500

    ..

--- frontend/src/app/pages/events/event-page/components/event-opportunities/event-opportunities.component.ts
+++ frontend/src/app/pages/events/event-page/components/event-opportunities/event-opportunities.component.ts
@@ -1,4 +1,6 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { EventOpportunity } from '@app/models/event/event-opportunity.model';
+import { getRegistrationContext } from '@app/models/event/event-registration.model';
 import { EventService } from '@app/pages/events/event-page/event.service';
 import { EventRegistrationService } from '@app/services/event-registration.service';
 import { EventOpportunityComponent } from "./event-opportunity/event-opportunity.component";
@@ -9,18 +11,20 @@ import { EventOpportunityComponent } from "./event-opportunity/event-opportunity
   templateUrl: './event-opportunities.component.html',
   styleUrl: './event-opportunities.component.scss'
 })
-export class EventOpportunitiesComponent {
+export class EventOpportunitiesComponent implements OnInit {
   private eventService = inject(EventService);
   private eventRegistrationService = inject(EventRegistrationService);
 
-  opportunities = computed(() => {
-    //const eventRegistrations = this.initialized ? [] : this.eventRegistrationService.userEventRegistrations();
-    const opportunities = this.eventService.eventData().opportunities
-    //   .filter((opp) => {
-    //   const context = getRegistrationContext(opp, eventRegistrations);
-    //   return !context;
-    // });
-    opportunities.sort((a, b) => new Date(a.startDate) < new Date(b.startDate) ? -1 : 1);
-    return opportunities.slice(0, 4);
-  });
+  opportunities = signal<EventOpportunity[]>([]);
+
+  ngOnInit(): void {
+    const eventRegistrations = this.eventRegistrationService.userEventRegistrations();
+    const availableOpportunities = this.eventService.eventData().opportunities
+      .filter((opp) => {
+        const context = getRegistrationContext(opp, eventRegistrations);
+        return !context;
+      });
+    availableOpportunities.sort((a, b) => new Date(a.startDate) < new Date(b.startDate) ? -1 : 1);
+    this.opportunities.set(availableOpportunities.slice(0, 4));
+  }
 }
