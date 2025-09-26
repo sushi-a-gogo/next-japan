077a58f7b00b11fa7a06dd85556e0a0fd96897a6
Joe Svatek <94716421+sushi-a-gogo@users.noreply.github.com>
2025-09-26 13:32:05 -0500

    Features/app enhancments (#87)
    * Add slide up animation
    
    * ..

--- frontend/src/app/components/sign-in-banner/sign-in-banner.component.ts
+++ frontend/src/app/components/sign-in-banner/sign-in-banner.component.ts
@@ -1,6 +1,9 @@
-import { afterNextRender, ChangeDetectionStrategy, Component, computed, inject, input, OnChanges, signal, SimpleChanges } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { afterNextRender, ChangeDetectionStrategy, Component, computed, DestroyRef, inject, input, OnChanges, OnInit, PLATFORM_ID, signal, SimpleChanges } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AuthService } from '@app/services/auth.service';
 import { LoginButtonComponent } from '@app/shared/login-button/login-button.component';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in-banner',
@@ -12,17 +15,35 @@ import { LoginButtonComponent } from '@app/shared/login-button/login-button.comp
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignInBannerComponent implements OnChanges {
+export class SignInBannerComponent implements OnInit, OnChanges {
   private auth = inject(AuthService);
+  private destroyRef = inject(DestroyRef);
+  private platformId = inject(PLATFORM_ID);
 
   remove = input<boolean>(false);
   isRemoved = signal<boolean>(false);
-  visible = computed(() => this.auth.loginStatus() === 'idle');
+  open = computed(() => this.auth.loginStatus() === 'idle');
+  visible = signal(false);
 
   constructor() {
     afterNextRender(() => {
       this.isRemoved.set(!!this.auth.user());
     });
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      fromEvent(window, 'scroll').pipe(
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe(() => {
+        const scrolled = window.scrollY || document.documentElement.scrollTop;
+        if (scrolled > 100) {
+          this.visible.set(true);
+        } else {
+          this.visible.set(false);
+        }
+      })
+    }
   }
 
   ngOnChanges(changes: SimpleChanges): void {
