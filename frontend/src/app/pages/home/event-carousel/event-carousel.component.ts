d95105b41e35f0dfd5ecfaea038deabb1ea20646
sushi-a-gogo <joe.svatek@icloud.com>
2025-10-07 16:04:11 -0500

    ..

--- frontend/src/app/pages/home/event-carousel/event-carousel.component.ts
+++ frontend/src/app/pages/home/event-carousel/event-carousel.component.ts
@@ -74,12 +74,18 @@ export class EventCarouselComponent implements OnChanges, AfterViewInit {
   private sortByDate(a: EventData, b: EventData) {
     const dateA = a.nextOpportunityDate ? new Date(a.nextOpportunityDate?.startDate) : null;
     const dateB = b.nextOpportunityDate ? new Date(b.nextOpportunityDate?.startDate) : null;
+    if (!dateA && !dateB) {
+      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+    }
+
     if (!dateA) {
       return 1;
     }
+
     if (!dateB) {
       return -1;
     }
+
     return dateA.getTime() - dateB.getTime();
   }
 
