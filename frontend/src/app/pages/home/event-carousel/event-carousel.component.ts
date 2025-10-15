b85d01460ecae1d47814cc706b903ce702a5ab73
sushi-a-gogo <joe.svatek@icloud.com>
2025-10-14 19:33:14 -0500

    ..

--- frontend/src/app/pages/home/event-carousel/event-carousel.component.ts
+++ frontend/src/app/pages/home/event-carousel/event-carousel.component.ts
@@ -1,75 +1,46 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, Component, computed, DestroyRef, ElementRef, inject, input, OnChanges, PLATFORM_ID, signal, SimpleChanges, ViewChild } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, computed, inject, input, PLATFORM_ID } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { EventData } from '@app/models/event/event-data.model';
-import { fromEvent } from 'rxjs';
+import { CarouselModule } from 'primeng/carousel';
+import { map } from 'rxjs';
 import { EventCardComponent } from './event-card/event-card.component';
-
-const CARD_WIDTH = 335; // 325px card + 10px gap
-
-const BreakpointsConfig = [
-  { query: '(max-width: 689.98px)', eventsPerView: 1 },
-  { query: '(min-width: 690px) and (max-width: 1014.98px)', eventsPerView: 2 },
-  { query: '(min-width: 1015px) and (max-width: 1339.98px)', eventsPerView: 3 },
-  { query: '(min-width: 1340px) and (max-width: 1664.98px)', eventsPerView: 4 },
-  { query: '(min-width: 1665px) and (max-width: 1989.98px)', eventsPerView: 5 },
-  { query: '(min-width: 1990px) and (max-width: 2314.98px)', eventsPerView: 6 },
-  { query: '(min-width: 2315px) and (max-width: 2500px)', eventsPerView: 7 },
-];
 
 @Component({
   selector: 'app-event-carousel',
   standalone: true,
-  imports: [EventCardComponent],
+  imports: [EventCardComponent, CarouselModule],
   templateUrl: './event-carousel.component.html',
   styleUrl: './event-carousel.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventCarouselComponent implements OnChanges, AfterViewInit {
-  @ViewChild('carouselTrack') carouselTrack!: ElementRef;
-  //@ViewChild("conveyer") conveyer!: NgxConveyerDirective;
-
-  animationState = signal('out');
-
-  events = input<EventData[]>([]);
-  sortedEvents = signal<EventData[]>([]);
-  eventsPerView = signal(1);
-  currentIndex = signal(0);
-
-  ssrMode = computed(() => !isPlatformBrowser(this.platformId));
-
-  private destroyRef = inject(DestroyRef);
+export class EventCarouselComponent {
   private platformId = inject(PLATFORM_ID);
   private breakpointObserver = inject(BreakpointObserver);
 
-  disablePrevButton = computed(() => this.currentIndex() === 0);
-  disableNextButton = computed(() => this.currentIndex() >= this.sortedEvents().length - this.eventsPerView());
+  events = input<EventData[]>([]);
+  sortedEvents = computed(() => {
+    const sorted = [...this.events().sort(this.sortByDate)];
+    return sorted
+  });
+  ssrMode = computed(() => !isPlatformBrowser(this.platformId));
+  showIndicators = toSignal(
+    this.breakpointObserver.observe(['(min-width: 1024px)']).pipe(
+      map(result => result.matches)
+    ),
+    { initialValue: isPlatformBrowser(this.platformId) ? this.breakpointObserver.isMatched('(min-width: 768px)') : false }
+  );
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['events']) {
-      this.sortedEvents.set([...this.events().sort(this.sortByDate)]);
-    }
-  }
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.setupBreakpoints();
-      const items = this.carouselTrack.nativeElement.querySelectorAll('.event-item');
-      this.animateIn(items);
-      this.setupScrollListener();
-    }
-  }
-
-  scrollPrev() {
-    const newIndex = Math.max(0, this.currentIndex() - this.eventsPerView());
-    this.scrollToIndex(newIndex);
-  }
-
-  scrollNext() {
-    const newIndex = Math.min(this.sortedEvents().length - this.eventsPerView(), this.currentIndex() + this.eventsPerView());
-    this.scrollToIndex(newIndex);
-  }
+  responsiveOptions = [
+    { breakpoint: '689.98px', numVisible: 1, numScroll: 1 },
+    { breakpoint: '1014.98px', numVisible: 2, numScroll: 1 },
+    { breakpoint: '1339.98px', numVisible: 3, numScroll: 2 },
+    { breakpoint: '1664.98px', numVisible: 4, numScroll: 3 },
+    { breakpoint: '1989.98px', numVisible: 5, numScroll: 4 },
+    { breakpoint: '2314.98px', numVisible: 6, numScroll: 5 },
+    { breakpoint: '2500px', numVisible: 7, numScroll: 6 },
+  ];
 
   private sortByDate(a: EventData, b: EventData) {
     const dateA = a.nextOpportunityDate ? new Date(a.nextOpportunityDate?.startDate) : null;
@@ -87,59 +58,5 @@ export class EventCarouselComponent implements OnChanges, AfterViewInit {
     }
 
     return dateA.getTime() - dateB.getTime();
-  }
-
-  private setupBreakpoints() {
-    this.breakpointObserver
-      .observe(BreakpointsConfig.map((bp) => bp.query))
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((result) => {
-        const activeBreakpoint = BreakpointsConfig.find((bp) => result.breakpoints[bp.query]);
-        const newEventsPerView = activeBreakpoint?.eventsPerView ?? 1;
-        this.eventsPerView.set(newEventsPerView);
-        if (newEventsPerView === 1) {
-          this.currentIndex.set(0);
-        }
-        this.scrollToIndex(this.currentIndex());
-      });
-  }
-
-  private setupScrollListener() {
-    if (isPlatformBrowser(this.platformId)) {
-      fromEvent(this.carouselTrack.nativeElement, 'scroll')
-        .pipe(takeUntilDestroyed(this.destroyRef))
-        .subscribe(() => {
-          const scrollLeft = this.carouselTrack.nativeElement.scrollLeft;
-          const newIndex = Math.ceil(scrollLeft / CARD_WIDTH);
-          if (newIndex !== this.currentIndex()) {
-            this.currentIndex.set(newIndex);
-          }
-        });
-    }
-  }
-
-  private animateIn(items: HTMLElement[]) {
-    requestAnimationFrame(() => {
-      this.animationState.set('in');
-      // Force repaint
-      requestAnimationFrame(() => {
-        items.forEach((item: HTMLElement, index: number) => {
-          const computedStyle = window.getComputedStyle(item);
-          item.style.transform = 'scale(1.0001)';
-          item.style.transform = '';
-        });
-      });
-    });
-  }
-
-  private scrollToIndex(index: number) {
-    if (isPlatformBrowser(this.platformId)) {
-      /*
-      -- pure css solution --
-      */
-      this.carouselTrack.nativeElement.scrollLeft = index * CARD_WIDTH;
-      // this.conveyer?.scrollTo(index * CARD_WIDTH, 500);
-      // this.currentIndex.set(index);
-    }
   }
 }
